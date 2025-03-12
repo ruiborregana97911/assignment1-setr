d2c513b0b87fb1837751b534221152a46bedbe67
heferreira1 <heferreira1@ua.pt>
2025-03-12 14:47:52 +0000

    make file run test

---
+++ MyDLL.c
@@ -0,0 +1,25 @@
+/** @file MyDLL_Module.c
+ * @brief Source file for the implementation of a Doubly Linked List (DLL)
+ * 
+ * MyDLL_Module.c has the implementations for the functions defined in the header file.
+ * 
+ * @authors Henrique Ferreira\n Rui Borregana
+ * @date 12 Mar 2025
+ * @bug -------------rever no fim-------------
+ */
+#include "MyDLL_Module.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+void MyDLLInit(list *dll){}
+
+void MyDLLInsert(list *dll, uint16_t key, const char *data){}
+
+void MyDLLRemove(list *dll, uint16_t id){}
+
+char* MyDLLFind(list *dll, uint16_t id){}
+
+char* MyDLLFindNext(list *dll, uint16_t id){}
+
+char* MyDLLFindPrevious(list *dll, uint16_t id){}
---
+++ apptest.c
@@ -0,0 +1,19 @@
+/** @file apptest.c
+ * @brief Test aplication for MyDLL_Module
+ * 
+ * apptest.c contains only the main function that provides the test
+ * 
+* @authors Henrique Ferreira\n Rui Borregana
+ * @date 12 Mar 2025
+ * @bug -------------rever no fim-------------
+ */
+
+#include "MyDLL.h"
+#include <string.h>
+#include <stdio.h>
+int main(){
+	
+	printf("apptest runned");
+	
+	return 1;
+	}
