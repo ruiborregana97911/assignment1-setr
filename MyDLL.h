30815d31327f580a7db8616823335919ffc63c1f
heferreira1 <heferreira1@ua.pt>
2025-03-12 14:35:38 +0000

    Update MyDLL.h
    v1.1

--- MyDLL.h
+++ MyDLL.h
@@ -26,95 +26,96 @@
 
 /**
  * @struct node
- * @brief Structure representing the elements in the Doubly Linked List
+ * @brief Structure representing a node in the doubly linked list
  *
- * This structure has one unique key chosen by the user that identifies the element itself, a char array that 
- * stores information called data and contains a reference to the previous and 
- * next element in the list. 
- * 
- */ 
+ * This structure contains a unique key assigned by the user to identify the node,
+ * a character array to store data, and references to the previous and next nodes in the list.
+ */
 typedef struct {
-	uint16_t key;	/**< Unique node identifier */
-	char data[MAX_SIZE_DATA]; /**< Data saved in the node */
-	int prev; /**< Key of the previous node */
-	int next; /**< Key of the next node */
-}node;
+    uint16_t key;              /**< Unique node identifier */
+    char data[MAX_SIZE_DATA];  /**< Data stored in the node */
+    int16_t prev;              /**< Key of the previous node (-1 for null) */
+    int16_t next;              /**< Key of the next node (-1 for null) */
+} node;
 /**
  * @struct list
  * @brief Structure representing the Doubly Linked List (DLL)
- * 
- * This structure stores all the elements, contains a reference to the 
- * first and last element and counts the number of elements on the list.
- * 
+ *
+ * This structure contains all the elements, a reference to the first and last nodes,
+ * and a counter for the number of nodes in the list. It also maintains an availability array
+ * to track free nodes.
  */
 typedef struct {
-	node nodelist[MAX_SIZE_NODE]; /**< Array of nodes in the list */
-	int head; /**< Key of the first node in the DLL */
-	int tail; /**< Key of the last node in the DLL */
-	int count; /**< Number of nodes in the DLL (node counter) */
-}list;
+    node nodelist[MAX_SIZE_NODE]; /**< Array of nodes in the list */
+    int head;                     /**< Key of the first node in the DLL */
+    int tail;                     /**< Key of the last node in the DLL */
+    int count;                    /**< Number of nodes in the DLL */
+    int available[MAX_SIZE_NODE];  /**< Array to track free nodes (1 = free, 0 = occupied) */
+} list;
 
 /**
  * @brief MyDLLInit initializes the Doubly Linked List
  *
- * This function initializes the list by setting the head and tail keys to -1 and the node counter to zero.
+ * This function initializes the list by setting the head and tail keys to -1,
+ * the node counter to zero, and marking all nodes as available.
  *
- *
- * @param[in] *dLL - Pointer to the list
- * @return Doesn't return anything
+ * @param[in] *dll Pointer to the list
  */
 void MyDLLInit(list *dll);
 /**
  * @brief MyDLLInsert adds an element to the Doubly Linked List
  *
- * This functions asks the user for a key and data then adds an element with it's own unique key and any size valid data into the Doubly Linked List
- * Also updates the node counter so its always equal or less than the MAX_SIZE_NODE and updadtes the head an tail Keys
+ * This function adds an element with a unique key and user-provided data into the DLL.
+ * It updates the node counter and adjusts the head and tail references accordingly.
  *
- * @param[in] *dLL - Pointer to the Doubly Linked List\n
- * @return Doesn't return anything
+ * @param[in] *dll Pointer to the DLL
+ * @param[in] key Unique identifier for the new node
+ * @param[in] *data Data to be stored in the node
  */
-void MyDLLInsert(list *dll);
+void MyDLLInsert(list *dll, uint16_t key, const char *data);
 /**
  * @brief MyDLLRemove removes an element/node from the Doubly Linked List
  *
- * This function removes and element from the list, identified by its key
- * Also updates the node and updadtes the head an tail Keys if necessary
- * 
- * @param[in] *dLL Pointer to the Doubly Linked List
- * @param[in] id Element identifier 
- * Doesn't return anything.
+ * This function removes a node identified by its key.
+ * It also updates the node counter and adjusts the head and tail references if necessary.
+ *
+ * @param[in] *dll Pointer to the DLL
+ * @param[in] id Identifier of the node to be removed
  */
 void MyDLLRemove(list *dll, uint16_t id);
 /**
  * @brief MyDLLFind returns a node from the Doubly Linked List
  *
- * This function askes the user for a key and returns the data of an element, identified by its key, or error if it does not exist
- * 
- * @param[in] *dLL Pointer to the Doubly Linked List
- * @return pointer to the intended node if the provided key is valid
- * @return NULL if the provided key is invalid
+ * This function searches for a node by its key and returns its data.
+ * If the node does not exist, it returns NULL.
+ *
+ * @param[in] *dll Pointer to the DLL
+ * @param[in] id Identifier of the node to find
+ * @return Pointer to the data if the node is found, NULL otherwise
  */
-char MyDLLFind(list *dll);
+char* MyDLLFind(list *dll, uint16_t id);
 /**
  * @brief MyDLLFindNext returns a node from the Doubly Linked List
  *
- * This function askes the user for a key and returns the data of the next element or error if it does not exist
- * 
- * @param[in] *dLL Pointer to the Doubly Linked List
- * @return pointer to the intended node if the provided key/id or keywords "next"/"prev" are valid
- * @return NULL if the provided key/id or keywords "next"/"prev" are invalid
+ * This function searches for a node by its key and returns the data of the next node.
+ * If the next node does not exist, it returns NULL.
+ *
+ * @param[in] *dll Pointer to the DLL
+ * @param[in] id Identifier of the current node
+ * @return Pointer to the data of the next node if it exists, NULL otherwise
  */
-char MyDLLFindNext(list *dll);
+char* MyDLLFindNext(list *dll, uint16_t id);
 /**
  * @brief MyDLLFindPrevious returns a node from the Doubly Linked List
  *
- * This function askes the user for a key and returns the data of the previous element or error if it does not exist
- * 
- * @param[in] *dLL Pointer to the Doubly Linked List
- * @return pointer to the intended node if the provided key/id or keywords "next"/"prev" are valid
- * @return NULL if the provided key/id or keywords "next"/"prev" are invalid
+ * This function searches for a node by its key and returns the data of the previous node.
+ * If the previous node does not exist, it returns NULL.
+ *
+ * @param[in] *dll Pointer to the DLL
+ * @param[in] id Identifier of the current node
+ * @return Pointer to the data of the previous node if it exists, NULL otherwise
  */
-char MyDLLFindPrevious(list *dll);
+char* MyDLLFindPrevious(list *dll, uint16_t id);
 
 #endif
 
